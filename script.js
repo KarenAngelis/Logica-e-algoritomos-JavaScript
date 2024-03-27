a7a739fe74a94dc26eb66a8d06fdbd65d0b0e7dd
Karen <karen.denich@gmail.com>
2024-03-27 14:05:04 -0300

    finalizado

--- script.js
+++ script.js
@@ -177,5 +177,275 @@ if (noteResult >= 6){
     alert (student + " estude para sua prova de recuperação! Sua média foi: " +  noteResult);
 }
 
+
+CRIANDO LISTA DE COMPRAS
+
+Capture 10 Itens para compor a lista de um supermercado.
+
+Após capturar os 10 itens. imprima-os, separando por vírgula.
+
+alert("Lista de supermercado com 10 itens")
+
+let items = []
+
+for (let item=0; item<10; item++){
+    let itemName= prompt("Digite o item: " + (item + 1))
+
+items [item] = itemName
+}
+
+alert(items)
+
+JOGO DE ADIVINHAÇÃO
+
+  ** Jogo da advinhação **
+
+  Apresente a mensagem ao usuário:
+  "Advinhe o número que estou pensando? Está entre 0 e 10"
+
+  Crie um lógica para gerar um número aleatório 
+  e verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
+
+  Enquanto o usuário não adivinhar o número, mostrar a mensagem:
+  "Erro, tente novamente:"
+
+  Caso o usuário acerte o número, apresentar a mensagem:
+  "Parabéns! Você advinhou o número em x tentativas"
+
+  Substitua o "x" da mensagem, pelo número de tentativas
+
+let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
+
+let xAttempts = 1
+
+while(Number(result) != randomNumber) {
+  result = prompt("Erro, tente novamente:")
+  xAttempts++
+}
+
+alert(`Parabéns! O número que eu pensei foi ${randomNumber} e você advinhou o número em ${xAttempts} tentativas`)
+
+ Faça um programa que tenha um menu e apresente a seguinte mensagem:
+  
+  Olá usuário! Digite o número da opção desejada
+
+    1. Cadastrar um item na lista
+    2. Mostrar itens cadastrados
+    3. Sair do programa
+  
+  --- 
+  O programa deverá capturar o número digitado pelo usuário e mostrar o seguintes cenários:
+
+  Caso o usuário digite 1, ele poderá cadastrar um item em uma lista
+  Caso o usuário digite 2, ele poderá ver os itens cadastrados
+    Se não houver nenhum item cadastrado, mostrar a mensagem: 
+      "Não existem itens cadastrados"
+  Caso o usuário digite 3, a aplicação deverá ser encerrada.
+
+
+  let option
+let items = []
+
+while(option != 3) {
+
+  option =  Number(prompt(`
+    Olá usuário! Digite o número da opção desejada
+    
+    1. Cadastrar um item na lista
+    2. Mostrar itens cadastrados
+    3. Sair do programa
+  `))
+  
+  
+  if(option == 1) {
+    let item = prompt("Digite o nome do item")
+    items.push(item) 
+  }
+  
+  else if (option == 2) {
+  
+    if(items.length == 0) {
+      alert("Não existem itens cadastrados")
+    } else {
+      alert(items)
+    }
+  
+  } else {
+    alert("Tchau")
+  }
+  
+  console.log(option, items)
+
+}
+
+
+OUTRA FORMA DE FAZER:
+
+/*
+
+  Faça um programa que tenha um menu e apresente a seguinte mensagem:
+  
+  Olá usuário! Digite o número da opção desejada
+
+    1. Cadastrar um item na lista
+    2. Mostrar itens cadastrados
+    3. Sair do programa
+  
+  --- 
+  O programa deverá capturar o número digitado pelo usuário e mostrar o seguintes cenários:
+
+  Caso o usuário digite 1, ele poderá cadastrar um item em uma lista
+  Caso o usuário digite 2, ele poderá ver os itens cadastrados
+    Se não houver nenhum item cadastrado, mostrar a mensagem: 
+      "Não existem itens cadastrados"
+  Caso o usuário digite 3, a aplicação deverá ser encerrada.
+
+
+
+let option
+let items = []
+
+while(option != 3) {
+
+  option =  Number(prompt(`
+    Olá usuário! Digite o número da opção desejada
+    
+    1. Cadastrar um item na lista
+    2. Mostrar itens cadastrados
+    3. Sair do programa
+  `))
+
+  switch(option) {
+    case 1:
+      let item = prompt("Digite o nome do item")
+      items.push(item) 
+      break
+    case 2:
+      if(items.length == 0) {
+        alert("Não existem itens cadastrados")
+      } else {
+        alert(items)
+      }
+      break
+    case 3: 
+      alert("Tchau")
+      break
+    default:
+      alert("Opção inválida. Tente novamente")
+  }
+
+}
+
+Estruturando dados com Objetos
+
+Crie uma lista de pacientes
+
+  Cada paciente dentro da lista, deverá conter
+    nome
+    idade
+    peso
+    altura
+
+  Escreva uma lista contendo o nome dos pacientes
+
+
+const patients = [
+  {
+    name: "Luiz",
+    age: 20,
+    weight: 100,
+    height: 190,
+  },
+  {
+    name: "Alexandra",
+    age: 27,
+    weight: 70,
+    height: 170,
+  },
+  {
+    name: "Carlos",
+    age: 42,
+    weight: 90,
+    height: 180,
+  },
+]
+
+let patientsNames = []
+
+for(let patient of patients) {
+  patientsNames.push(patient.name)
+}
+
+alert(patientsNames)
+
+
+CALCULAR IMC
+ Dada uma lista de pacientes, descubra o IMC de cada paciente e imprima
+
+  "Paciente X possui o IMC de: Y"
+
+  Onde X é o nome do paciente e Y é o IMC desse paciente
+
+  Crie uma função para fazer o cálculo de IMC
+
+ peso / (altura * altura) *
+
+const patients = [
+  {
+    name: "Luiz",
+    age: 20,
+    weight: 100,
+    height: 190
+  },
+  {
+    name: "Alexandra",
+    age: 27,
+    weight: 70,
+    height: 170
+  },
+  {
+    name: "Carlos",
+    age: 42,
+    weight: 90,
+    height: 180
+  },
+]
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
+function printPatientIMC(patient) {
+  return `
+    Paciente ${patient.name} possui o IMC de
+    ${IMC(patient.weight, patient.height)}
+  `
+}
+
+for (let patient of patients) {
+  let IMCmessage = printPatientIMC(patient)
+  alert(IMCmessage)
+}
+
 */
 
+
+
+let firstNumber = prompt('Digite o primeiro número')
+let secondNumber = prompt('Digite o segundo número')
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const multi = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restDiv = firstNumber % secondNumber
+
+alert('Soma: ' + sum)
+alert('Subtração: ' + sub)
+alert('Multiplicação: ' + multi)
+alert('Divisão: ' + div)
+alert('Resto da divisão: ' + restDiv)
